788440ddd0bdf4ef5539cdc0dc07886fdce25413
Panji Hanum <83002940+panjihanumz@users.noreply.github.com>
2021-12-02 03:04:45 +0700

    handle Security

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+]
+
 module.exports = {
   reactStrictMode: true,
   webpack(config) {
@@ -6,7 +37,16 @@ module.exports = {
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-    
+
     return config
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
